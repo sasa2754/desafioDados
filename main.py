fef79087cbf46d2b39be280e1621aeed5c41ac5c
sasa2754 <sasamor5@gmail.com>
2024-11-01 11:35:15 -0300

    agr peguei só as bordas, falta pegar as bolinhas certo também

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ import cv2 as cv
 import numpy as np
 
 # Carregar a imagem do labirinto com os contornos em vermelho
-labirinto = cv.imread('labirintoContornado.jpg')
+labirinto = cv.imread('labirintoContornado2.jpg')
 
 # Verificar se a imagem foi carregada corretamente
 if labirinto is None:
     print("Erro ao carregar a imagem.")
     exit()
 
+
 # Converter a imagem para escala de cinza
 labirinto_gray = cv.cvtColor(labirinto, cv.COLOR_BGR2GRAY)
 
+kernel = np.ones((5,5),np.uint8)
+labMorph = cv.morphologyEx(labirinto_gray, cv.MORPH_OPEN, kernel)
+
+# cv.imshow('Labirinto com morfologia', labMorph)
+
 # Aplicar um filtro para suavizar a imagem
-blur = cv.medianBlur(labirinto_gray, 5)
+blur = cv.medianBlur(labMorph, 5)
 
 # Usar Canny para detectar bordas
 bordas = cv.Canny(blur, 50, 150)
 
-# Encontrar todos os contornos na imagem original
-contornos, _ = cv.findContours(bordas, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+# Criar a máscara - ajuste os limites conforme necessário
+mask = cv.inRange(labMorph, 100, 200)  # Ajuste esses valores conforme a iluminação da imagem
+
+# Encontrar todos os contornos na máscara
+contornos, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+# cv.imshow('Bordas', bordas)
 
 # Imprimir a quantidade de contornos encontrados
 print(f"Total de contornos encontrados: {len(contornos)}")
@@ -29,13 +40,13 @@ for contorno in contornos:
     area = cv.contourArea(contorno)
     
     # Limite para tentar pegar todos os dados
-    if 1 < area < 9000:  
+    if 200 < area < 9000:  # Aumentei o limite mínimo da área para evitar detecção de pequenos contornos
         # Obter o retângulo delimitador do dado
         x, y, w, h = cv.boundingRect(contorno)
 
         # Verificar a proporção para considerar se é um dado
         aspect_ratio = float(w) / h
-        if (0.5 < aspect_ratio < 2) and area > 10:  # Filtrar por proporção e área
+        if (0.5 < aspect_ratio < 2):  # Filtrar por proporção
             # Desenhar o retângulo delimitador em verde
             cv.rectangle(labirinto, (x, y), (x + w, y + h), (0, 255, 0), 2)
             
